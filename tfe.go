c5eee0211e453d0efbcca18b1f03a4244b714313
Omii Moore <omoore@fitbit.com>
2020-09-29 17:00:49 -0700

    debug

--- tfe.go
+++ tfe.go
@@ -582,9 +582,8 @@ func (c *Client) do(ctx context.Context, req *retryablehttp.Request, v interface
 
 	//if a != reflect.Zero() {
 	//	println("inside before")
-	fmt.Printf("%+v\n", &m.StatusCounts)
 	fmt.Printf("%+v\n", m.StatusCounts)
-	statusCounts.Set(reflect.ValueOf(&m.StatusCounts))
+	statusCounts.Set(reflect.ValueOf(m.StatusCounts))
 	//	println("inside after")
 	//}
 	println("outside after")
